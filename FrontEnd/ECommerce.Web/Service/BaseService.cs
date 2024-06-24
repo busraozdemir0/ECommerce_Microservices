26bd8429e55f4dd9376e2e531bae13c7aca9a7a3
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-06-24 16:15:22 +0300

    Kupon işlemleri için servis kısmının FrontEnd tarafında yazılması

---
+++ FrontEnd/ECommerce.Web/Service/BaseService.cs
@@ -0,0 +1,90 @@
+﻿using ECommerce.Services.Models;
+using ECommerce.Web.Models;
+using ECommerce.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static ECommerce.Web.Utility.SD;
+
+namespace ECommerce.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        // Api ile haberlesmek icin kullanilmaktadir
+        public async Task<ResponseDTO?> SendAsync(RequestDTO requestDTO)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("ECommerceAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                // token
+
+                message.RequestUri = new Uri(requestDTO.Url);
+                if (requestDTO.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDTO.Data), Encoding.UTF8, "application/json");
+
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+                switch (requestDTO.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
+                        return apiResponseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDTO
+                {
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false
+                };
+                return dto;
+            }
+        }
+    }
+}
