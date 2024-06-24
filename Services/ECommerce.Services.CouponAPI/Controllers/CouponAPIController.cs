2026cf493963e80865889747b736fb1079e73c6e
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-06-24 14:20:35 +0300

    Add project files.

---
+++ Services/ECommerce.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,52 @@
+﻿using ECommerce.Services.CouponAPI.Data;
+using ECommerce.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CouponAPIController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _context.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")] // Bu sekilde route'unu belirtmezsek swagger'da hata aliriz.
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon value = _context.Coupons.First(u => u.CouponId == id);
+                return value;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
