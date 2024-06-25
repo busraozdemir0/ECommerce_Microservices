82fbb995734514ef09724f7eb4958c61016a6e87
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-06-25 14:44:50 +0300

    Login & Register işlemlerinin Back-end'de tamamlanması + Token üretme, Rol atama

--- Services/ECommerce.Services.AuthAPI/Data/AppDbContext.cs
+++ Services/ECommerce.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ECommerce.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Services.AuthAPI.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Services/ECommerce.Services.AuthAPI/Program.cs
+++ Services/ECommerce.Services.AuthAPI/Program.cs
@@ -1,4 +1,7 @@
 using ECommerce.Services.AuthAPI.Data;
+using ECommerce.Services.AuthAPI.Models;
+using ECommerce.Services.AuthAPI.Service;
+using ECommerce.Services.AuthAPI.Service.IService;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,10 +13,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
